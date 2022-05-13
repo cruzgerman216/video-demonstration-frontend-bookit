fa9471172f784947c8d592f0130eff4a8c2fb21f
German Cruz <43277807+cruzgerman216@users.noreply.github.com>
2022-05-13 10:11:12 -0500

    delete

--- src/app/shared/http/http.service.ts
+++ src/app/shared/http/http.service.ts
@@ -6,27 +6,23 @@ import { tap } from 'rxjs/operators';
 
 @Injectable({ providedIn: 'root' })
 export class HttpService {
-  // * VARIABLES * \\
-  firebaseRootURL =
-    'https://cape-book-app-default-rtdb.firebaseio.com/books.json';
-
-  // * INJECTIONS * \\
   constructor(
     private http: HttpClient,
     private bookshelfService: BookshelfService
   ) {}
 
-  // * METHODS * \\
-  //   Save books to Firebase DB
-  saveBooksToFirebase() {
-    const books = this.bookshelfService.getBooks();
-
-    this.http.put(this.firebaseRootURL, books).subscribe((res) => {
-      console.log('Firebase DB Response:', res);
-    });
+  saveBook(book) {
+    this.http
+      .post('https://paducah-bookit-api.herokuapp.com/api/v1/books', book)
+      .subscribe((res: any) => {
+        console.log('Firebase DB Response:', res);
+        // Update the view
+        if (res.success) {
+          this.bookshelfService.saveBook(res.payload.book);
+        }
+      });
   }
 
-  // Fetch books from Firebase DB
   fetchBooks() {
     return this.http
       .get('https://paducah-bookit-api.herokuapp.com/api/v1/books/my_books')
